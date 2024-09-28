a07bf9a28028c25514a1611ccc35dd0a7141b571
AsepMaulanaMalik <maulanaasep5351@gmail.com>
2024-09-28 14:41:05 +0700

    Add files via upload

---
+++ tugas 1 luas segitiga.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+		int panjang = 8;
+		int tinggi = 5;
+		float luas;
+		
+		luas = 0.5 * panjang * tinggi;
+		
+		printf("luas segitiga adalah: %.f cm\n", luas);
+		
+		return 0;
+}
